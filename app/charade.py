7ef0d5214ee48b3623e5e621f05d6ce22c3f7bf5
Dean Stamler <dean@stamler.ca>
2018-06-11 10:27:43 -0400

    wrap init of app in create() function to facilitate testing

--- app/charade.py
+++ app/charade.py
@@ -11,19 +11,26 @@ from database import db_obj
 from Resource import Resource
 from middleware import AzureADTokenValidator, CORSComponent, CacheController
 
-# Initialize validation middleware
-azure_cfg = config.config['azure_ad']
-validator = AzureADTokenValidator(azure_cfg['tenant_name'], azure_cfg['app_id'])
-
-# Initialize other middleware
-cors = CORSComponent()
-cache_controller = CacheController()
-
 # Create the falcon API instance (a WSGI app)
-app = falcon.API(middleware = [ cors, validator, cache_controller ])
+def create():
 
-# instantiate resources, hook up routes and store references to them in db_obj
-for _, res_config in db_obj.resources.items():
-    resource = Resource(res_config)
-    for uri in res_config['URIs']:
-        app.add_route(uri, resource)
+    # Initialize validation middleware
+    azure_cfg = config.config['azure_ad']
+    validator = AzureADTokenValidator(azure_cfg['tenant_name'], azure_cfg['app_id'])
+
+    # Initialize other middleware
+    cors = CORSComponent()
+    cache_controller = CacheController()
+
+    app = falcon.API(middleware = [ cors, validator, cache_controller ])
+
+    # instantiate resources, hook up routes and store references to them in db_obj
+    for _, res_config in db_obj.resources.items():
+        resource = Resource(res_config)
+        for uri in res_config['URIs']:
+            app.add_route(uri, resource)
+
+    return app
+
+# uwsgi.ini expects callable named 'app'
+app = create()
