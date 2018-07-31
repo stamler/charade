03b4030a6bd72731510fdc47ca732c6a8596bd02
Dean Stamler <dean@stamler.ca>
2018-07-31 16:19:59 -0400

    test_app initial commit

---
+++ tests/test_app.py
@@ -0,0 +1,88 @@
+# library imports
+import falcon
+import pytest
+import json
+from falcon import testing
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+# project imports
+from charade import sentinel
+from charade import model
+from charade import app as a
+
+# testing imports
+from .assertions import assert_valid_schema
+from .config import cfg
+
+# Test configuration.
+authed_header = {
+        "Accept": "application/vnd.api+json", 
+        "Authorization": cfg['Token'],
+        "Content-Type": "application/vnd.api+json" 
+}
+
+# Initialize the app for testing
+api = a.create(cfg)
+
+# Initialize the database (from sentinel.py and model.py) here
+engine = create_engine(cfg['db'])
+# TODO: fold these into init_sentinel_tables() and init_model_tables()
+# and include verification of emptiness prior to create_all()
+model.Base.metadata.create_all(engine)
+sentinel.Base.metadata.create_all(engine)
+session = Session(engine)
+sentinel.init_sentinel_tables(session)
+
+@pytest.fixture
+def client():
+    return testing.TestClient(api)
+
+# Mock empty database that matches production parameters
+
+# G1: Test all methods on root, both authenticated and unauthenticated 
+def test_root_unauthenticated(client):
+    response = client.simulate_request('GET','/', headers={
+        "Accept": "application/vnd.api+json"})
+    assert response.headers['Content-Type'] == "application/vnd.api+json"
+    assert response.status == falcon.HTTP_UNAUTHORIZED
+    assert_valid_schema(response.json,"jsonapi.schema.json")
+
+def test_root_authenticated(client):
+    response = client.simulate_request('GET','/', headers=authed_header)
+    assert response.headers['Content-Type'] == "application/vnd.api+json"
+    assert response.status == falcon.HTTP_OK
+    assert_valid_schema(response.json,"jsonapi.schema.json")
+    assert_valid_schema(response.json, "root.schema.json" )
+
+# P1: Test POST of valid single object
+def test_post_location(client):
+    # Post body
+    P1 = { 
+            "data": 
+                { 
+                    "name": "Test Location", 
+                    "address": "123 Sesame St", 
+                    "city": "Anytown" 
+                }
+        }
+
+    response = client.simulate_request('POST', '/Locations', 
+        headers=authed_header,
+        body=json.dumps(P1) # use body since json overwites Content-Type in header
+        )
+    assert response.headers['Content-Type'] == "application/vnd.api+json"
+    assert response.status == falcon.HTTP_201
+
+# P2: Test POST multiple objects less than max_multi
+# P3: Test POST multiple objects greater than max_multi
+# G2: Test GET data POSTed in P1 is correct
+# G3: Test GET data POSTed in P2 is correct
+# G4: Test GET data POSTed in P3 is correct
+# G5: Test GET all resources number matches
+# D1: Test DELETE first item from P2
+# G6: Test GET item deleted in D1, confirm it is gone
+# D2: Test DELETE item from P1
+# D3: Test DELETE all non-first items from P2
+# D4: Test DELETE all items from P3
+# G7: Test GET all resources returns zero
